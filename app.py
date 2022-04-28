cfd40827156442ef74af91f83a0d929f4de36198
alopanik <alopanik@gmail.com>
2022-04-27 21:32:24 -0400

    Adding EVShopper.

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ app = Flask(__name__)
 def index():
     return render_template('home.html')
 
+@app.route('/evshopper')
+def evshopper():
+    return render_template('evshopper.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
